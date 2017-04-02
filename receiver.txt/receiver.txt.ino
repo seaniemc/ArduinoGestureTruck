9744965431d1118f248db082b6fb5dd5f73a12e0
Sean McGrath <Sean McGrath>
2017-04-02 03:53:28 +0100

    updated the motor to try slow down current, unsuccessful

--- receiver.txt/receiver.txt.ino
+++ receiver.txt/receiver.txt.ino
@@ -1,12 +1,14 @@
 
 //int xPin=0;//Connect x pin of adxl335 to pin A0
 //int yPin=1;//Connect y pin of adxl335 to pin A1
+int slm=6;
+//int srm=5;
 
 int lm=9;
 int lmr=8;
 int rm=10;
 int rmr=7;
-int rled=5;
+//int rled=5;
 int ledPin=13;//led on pin 13 is ON except when bot is stationary
 
 void setup()
@@ -18,7 +20,9 @@ void setup()
  pinMode(lmr,OUTPUT);
  pinMode(rm,OUTPUT);
  pinMode(rmr,OUTPUT);
-pinMode(rled,OUTPUT);
+//pinMode(rled,OUTPUT);
+pinMode(slm,OUTPUT);
+//pinMode(srm,OUTPUT);
 }
 void loop()
 {
@@ -33,10 +37,14 @@ void loop()
 
   //delay(1000); //used to display values after 2s delay
   Serial.print("\n");//print after 2s in a new line
-    if (xval > 305 && xval < 360)//Stationary
+    if (xval > 325 && xval < 345)//Stationary
     {
       stationary();
     }
+    if(xval > 345 && xval < 360)//Forward
+      {
+        slowForward();
+      }
       if(xval > 360)//Forward
       {
         forward();
@@ -58,7 +66,7 @@ void loop()
       }
       
 //    Serial.print("\n");// debugging
-//        delay(1000);
+        delay(1000);
 }
 
 /////////////////////////////////////////////////
@@ -69,18 +77,29 @@ void forward()
   digitalWrite(lmr,HIGH);
   digitalWrite(rm,HIGH);
   digitalWrite(rmr,LOW);
-  Serial.print("FORWARD");
+  
+  digitalWrite(slm,HIGH);
+  Serial.print("FORWARD  ");
   
   digitalWrite(ledPin,HIGH);
 }
-
+void slowForward()
+{
+  digitalWrite(lm,LOW);  
+  digitalWrite(lmr,HIGH);
+  digitalWrite(rm,HIGH);
+  digitalWrite(rmr,LOW);
+  Serial.print("FORWARD  ");
+  digitalWrite(slm,LOW);
+  
+}
 void back()
 {
    digitalWrite(lm,HIGH);  
     digitalWrite(lmr,LOW);
     digitalWrite(rm,LOW);
     digitalWrite(rmr,HIGH);
-    
+    digitalWrite(slm,LOW);
     digitalWrite(ledPin,HIGH);
 }
 
@@ -90,6 +109,7 @@ void left(){
   digitalWrite(lmr,LOW);
   digitalWrite(rm,LOW);
   digitalWrite(rmr,LOW);
+  digitalWrite(slm,LOW);
   digitalWrite(ledPin,HIGH);
 }
 
@@ -99,7 +119,7 @@ void right()
   digitalWrite(lmr,LOW);
   digitalWrite(rm,HIGH);
   digitalWrite(rmr,LOW);
-  
+  digitalWrite(slm,LOW);
   Serial.print("RIGHT");
 }
 
@@ -108,5 +128,6 @@ void stationary(){
   digitalWrite(lmr,LOW);
   digitalWrite(rm,LOW);
   digitalWrite(rmr,LOW);
+  digitalWrite(slm,LOW);
 }
 
